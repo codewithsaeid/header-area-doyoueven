32b10b93f091fd83d8163f00f5f693cd1900ac38
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-03-17 10:29:01 +0000

    home page slider and topbar hinding

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+/** mobile nav */
 $(document).ready(function () {
     $('.sidenav').sidenav();
 });
+/**slider */
+
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+
+/** hiding topbar */
+
+$(window).scroll(function() {
+if ($(this).scrollTop()>0)
+{
+$('.top-navbar').hide(100);
+}
+else
+{
+$('.top-navbar').show(200);
+}
+});
+
+/** */
+
