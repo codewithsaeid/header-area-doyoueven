4e2f27fb938ea89144f2b14349ec4a7de6281fff
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-03-20 02:52:01 -1200

    home page slider

--- js/main.js
+++ js/main.js
@@ -1,25 +1,46 @@
 /** mobile nav */
 $(document).ready(function () {
-    $('.sidenav').sidenav();
+  $('.sidenav').sidenav();
 });
-/**slider */
-
-$(document).ready(function(){
-  $('.carousel').carousel();
-});
+/** cart nav */
+// $(document).ready(function () {
+//     $('.cartmenu').sidenav();
+// });
 
 /** hiding topbar */
 
-$(window).scroll(function() {
-if ($(this).scrollTop()>0)
-{
-$('.top-navbar').hide(100);
-}
-else
-{
-$('.top-navbar').show(200);
-}
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 0) {
+    $('.top-navbar').hide(100);
+  } else {
+    $('.top-navbar').show(200);
+  }
 });
+/**slider */
 
-/** */
-
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true
+});
+$('.next').click(function () {
+  $('.carousel.carousel-slider').carousel('next');
+});
+$('.prev').click(function () {
+  $('.carousel.carousel-slider').carousel('prev');
+});
+/** text-text slider controler */
+$('.slider').slider({
+  full_width: true,
+  interval: 2000,
+  height:50,
+  indicators: false
+});
+$('.text-next').click(function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  $('.slider').slider('next');
+});
+$('.text-prev').click(function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  $('.slider').slider('prev');
+});
